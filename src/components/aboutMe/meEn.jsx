7b676a8d6c3cd821db4d5b8607da0d418eba3756
thomas bartier <thomas.bartier@gmail.com>
2023-10-13 15:29:37 +0200

    about me components trad

---
+++ src/components/aboutMe/meEn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const meEn = () => {
+  return (
+    <div>
+    <p>Après avoir découvert le développement en mai 2022 pendant 5 semaines, je suis partie sur une <b>CDA</b> (<i>formation 
+      concepteur développeur d'application</i>) durant 8 mois.</p>
+      <br />
+      <p>Après <b>obtention du diplôme</b> j'ai intégré un incubateur et une équipe pendant 4 mois où j'ai 
+      travaillé sur un projet de covoiturage (CoDrive) en mise en situation professionnel 
+      (<a href="#"><i className="hover:underline cursor-pointer">voir mes experiences</i></a>).</p>   
+      <br />     
+      <p><b>Aujourd'hui</b>, je suis en recherche d'emploi et je suis <b>disponible immédiatement</b>.
+      </p>
+    </div>
+  )
+}
+
+export default meEn
