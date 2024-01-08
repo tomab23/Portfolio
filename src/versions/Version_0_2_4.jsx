e54723d61eb730e1f229227a8863e40c3b5b82e3
thomas bartier <thomas.bartier@gmail.com>
2024-01-08 10:30:52 +0100

    version 0.2.4 beta

--- src/versions/Version_0_2_4.jsx
+++ src/versions/Version_0_2_4.jsx
@@ -6,11 +6,11 @@ const Version_0_2_4 = () => {
           <div>
       {localStorage.getItem("i18nextLng") === "en" ? (
         <div>
-          <p>- Change the navigation bar on mobile phones </p>
+          <p>- Change the navigation bar on mobile phones(beta) </p>
         </div>
       ) : (
         <div>
-          <p>- Modification de la barre de navigation sur téléphone mobile </p>
+          <p>- Modification de la barre de navigation sur téléphone mobile (beta) </p>
         </div>
       )}
     </div>
