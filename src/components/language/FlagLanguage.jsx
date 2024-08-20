114a020fe7cf741063610651eb7e85e1dd4c1fbf
thomas bartier <thomas.bartier@gmail.com>
2024-08-20 17:17:35 +0200

    add ReactCountryFlag

--- src/components/language/FlagLanguage.jsx
+++ src/components/language/FlagLanguage.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React from "react";
 import flagFr from "../../assets/images/flags/Flag_of_France.svg";
 import flagEn from "../../assets/images/flags/Flag_of_the_United_Kingdom.svg";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import ReactCountryFlag from "react-country-flag";
 
 const FlagLanguage = ({ language }) => {
-
   const { t } = useTranslation();
 
-    const text = language === "en" ? "EN" : "FR"
+  const text = language === "en" ? "EN" : "FR";
 
   return (
     <div>
       <div>
-          <div className="flex items-center gap-1">
-            <img src={language === "en" ? flagEn : flagFr} 
-            alt={language === "en" ? t("flag.en") : t("flag.fr")} className="h-3" />
-            <p className="text-sm">{text}</p>
-          </div>
+        <div className="flex items-center gap-1">
+          {/* <img src={language === "en" ? flagEn : flagFr} 
+            alt={language === "en" ? t("flag.en") : t("flag.fr")} className="h-3" /> */}
+          {language === "en" ? (
+            <ReactCountryFlag
+              countryCode="GB"
+              svg
+              alt={t("flag.en")}
+              title={t("flag.en")}
+            />
+          ) : (
+            <ReactCountryFlag
+              countryCode="FR"
+              svg
+              alt={t("flag.fr")}
+              title={t("flag.fr")}
+            />
+          )}
+          <p className="text-sm">{text}</p>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default FlagLanguage
+export default FlagLanguage;
--- src/components/language/LangueChoose.jsx
+++ src/components/language/LangueChoose.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import flagFr from "../../assets/images/flags/Flag_of_France.svg";
-import flagEn from "../../assets/images/flags/Flag_of_the_United_Kingdom.svg"
+import flagEn from "../../assets/images/flags/Flag_of_the_United_Kingdom.svg";
 import { useTranslation } from "react-i18next";
+import ReactCountryFlag from "react-country-flag";
+<ReactCountryFlag countryCode="US" svg />;
 
 const LangueChoose = () => {
-
   const { t } = useTranslation();
 
   return (
@@ -12,14 +13,26 @@ const LangueChoose = () => {
       <div>
         {localStorage.getItem("i18nextLng") === "en" ? (
           <div className="flex items-center gap-1">
-            <img src={flagEn} 
-            alt={t("flag.en")} className="h-3" />
+            {/* <img src={flagEn} 
+            alt={t("flag.en")} className="h-3" /> */}
+            <ReactCountryFlag
+              countryCode="GB"
+              svg
+              alt={t("flag.en")}
+              title={t("flag.en")}
+            />
             <p className="text-sm">EN</p>
           </div>
         ) : (
           <div className="flex items-center gap-1">
-            <img src={flagFr}
-            alt={t("flag.fr")} className="h-3"/>
+            {/* <img src={flagFr}
+            alt={t("flag.fr")} className="h-3"/> */}
+            <ReactCountryFlag
+              countryCode="FR"
+              svg
+              alt={t("flag.fr")}
+              title={t("flag.fr")}
+            />
             <p className="text-sm">FR</p>
           </div>
         )}
